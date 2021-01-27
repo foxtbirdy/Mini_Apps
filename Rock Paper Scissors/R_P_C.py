27928857ab492644cc6432e6d6e5f155ee2d6e17
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2021-01-27 21:08:45 +0600

    Update R_P_C.py

--- Rock Paper Scissors/R_P_C.py
+++ Rock Paper Scissors/R_P_C.py
@@ -1,11 +1,71 @@
 import random
 
+INTRO = ("""
+:'######::::::'###::::'##::::'##:'########:::::'#######::'##::: ##:'####:
+'##... ##::::'## ##::: ###::'###: ##.....:::::'##.... ##: ###:: ##: ####:
+ ##:::..::::'##:. ##:: ####'####: ##:::::::::: ##:::: ##: ####: ##: ####:
+ ##::'####:'##:::. ##: ## ### ##: ######:::::: ##:::: ##: ## ## ##:: ##::
+ ##::: ##:: #########: ##. #: ##: ##...::::::: ##:::: ##: ##. ####::..:::
+ ##::: ##:: ##.... ##: ##:.:: ##: ##:::::::::: ##:::: ##: ##:. ###:'####:
+. ######::: ##:::: ##: ##:::: ##: ########::::. #######:: ##::. ##: ####:
+:......::::..:::::..::..:::::..::........::::::.......:::..::::..::....::
+
+""")
+
+DRAW = ("""
+.########..########.....###....##......##
+.##.....##.##.....##...##.##...##..##..##
+.##.....##.##.....##..##...##..##..##..##
+.##.....##.########..##.....##.##..##..##
+.##.....##.##...##...#########.##..##..##
+.##.....##.##....##..##.....##.##..##..##
+.########..##.....##.##.....##..###..###.
+""")
+
+Rock = 	("""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+
+""")
+
+
+Paper = ("""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+
+""")
+
+Scissors = ("""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+
+""")
+
+BORDER = ("""
+==================================
+                VS
+==================================
+""")
+
+
 options = {
 	"Rock",
-	"Paper",
+	"Paper", 
 	"Scissors"
 	}
-
+print(INTRO)
 while True:
 	player_ipt = input("What do you choice? > ")
 
@@ -13,29 +73,60 @@ while True:
 	com_result = random.choice(list(options))
 
 	if player_ipt == com_result:
-		print("Draw, The Opponent choice " + com_result + " as well")
+		print(DRAW)
+		print("\nDraw, The Opponent choice " + com_result + " as well")
 	elif player_ipt != com_result:
 
-	# Rock VS Paper
+	# Rock VS Paper - Fail!
 		if player_ipt == "Rock" and com_result == "Paper":
-			print("Failed! The Opponent scored " + com_result)
-	# Paper VS Rock
+			print("\nPlayer chose " + player_ipt)
+			print(Rock)
+			print(BORDER)
+			print("Computer chose " + com_result)
+			print(Paper)
+			print("\nComputer Win! , Player Lose!")
+	# Paper VS Rock - Score!
 		elif player_ipt == "Paper" and com_result == "Rock":
-			print("Scored! The Opponent scored " + com_result)
-	# Paper VS Scissors
+			print("\nPlayer chose " + player_ipt)
+			print(Paper)
+			print(BORDER)
+			print("\nComputer chose " + com_result)
+			print(Rock)
+			print("\nPlayer Win! , Computer Lose!")
+	# Paper VS Scissors - Fail!
 		elif player_ipt == "Paper" and com_result == "Scissors":
-			print("Failed! The Opponent scored " + com_result)
-	# Scissors VS Paper
+			print("\nPlayer chose " + player_ipt)
+			print(Paper)
+			print(BORDER)
+			print("\nComputer chose " + com_result)
+			print(Scissors)
+			print("\nComputer Win! , Player Lose!")
+	# Scissors VS Paper - Score!
 		elif player_ipt == "Scissors" and com_result == "Paper":
-			print("Scored! The Opponent scored " + com_result)	
-	# Scissors VS Rock 
+			print("\nPlayer chose " + player_ipt)
+			print(Scissors)
+			print(BORDER)
+			print("\nComputer chose " + com_result)
+			print(Paper)
+			print("\nPlayer Win! , Computer Lose!")
+	# Scissors VS Rock - Fail!
 		elif player_ipt == "Scissors" and com_result == "Rock":
-			print("Failed! The Opponent scored " + com_result)
-	# Rock VS Scissors 
+			print("\nPlayer chose " + player_ipt)
+			print(Scissors)
+			print(BORDER)
+			print("\nComputer chose " + com_result)
+			print(Rock)
+			print("\nComputer Win! , Player Lose!")
+	# Rock VS Scissors - Score!
 		elif player_ipt == "Rock" and com_result == "Scissors":
-			print("Scored! The Opponent scored " + com_result)
+			print("\nPlayer chose " + player_ipt)
+			print(Rock)
+			print(BORDER)
+			print("\nComputer chose " + com_result)
+			print(Scissors)
+			print("\nPlayer Win! , Computer Lose!")
 
 		else:
-			print("Error 2")
+			print("Please use Scissors,Rock,Paper")
 	else:
-		print("Error 1")
+		print("Error 1 = NANI!")
