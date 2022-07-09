a493203735e032082aa41692b2c22ea9f314899a
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2022-07-10 01:20:13 +0600

    removed unnecessary code. updated positions of the designs.

--- Temperature-Converter/app.py
+++ Temperature-Converter/app.py
@@ -2,14 +2,13 @@
 # @Author: Climax
 # @Date:   2022-07-09 22:31:13
 # @Last Modified by:   Climax
-# @Last Modified time: 2022-07-10 01:07:02
+# @Last Modified time: 2022-07-10 01:19:25
 
 
 import sys 
 
-from PyQt5.QtWidgets import (QMainWindow, QApplication, QLabel, QVBoxLayout)
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QLabel)
+from PyQt5.QtCore import Qt
 app = QApplication(sys.argv)
 
 
@@ -17,29 +16,28 @@ class MainWindow(QMainWindow):
 	def __init__(self):
 		super().__init__()	
 		self.setWindowTitle("Testing")
-		self.layout = QVBoxLayout()
-		self.setLayout(self.layout)
 		self.setFixedSize(600,800)
 		self.label_above()
 
 
 	def label_above(self):
+		# first label
 		label_Intro = QLabel("Enter your Temperature value below~", self)
 		font = label_Intro.font()
-		font.setPointSize(17)
+		font.setPointSize(20)
 		label_Intro.setFont(font)
-		label_Intro.setStyleSheet("border : 2px solid black;")
-		label_Intro.resize(400,20)
-		label_Intro.move(20,10)
+		label_Intro.resize(450,30)
+		label_Intro.move(20,20)
 
+		# second label
 		label_description = QLabel("Please insert your value in one of the boxes given below. The application will display the converted value of the other corresponding values.", self)
 		label_descript_font = label_description.font()
-		label_descript_font.setPointSize(17)
+		label_descript_font.setPointSize(15)
 		label_description.setFont(label_descript_font)
-		label_description.setStyleSheet("border : 2px solid black;")
-		label_description.resize(600,100)
-		label_description.move(20,45)
+		label_description.resize(550,90)
+		label_description.move(20,50)
 		label_description.setWordWrap(True)
+
 
 
 window = MainWindow()
