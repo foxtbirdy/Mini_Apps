683346c854a2ed90dbe1f304644bdcc7c403aaa4
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2022-07-17 01:01:54 +0600

    minor design changes

--- Temperature-Converter/app.py
+++ Temperature-Converter/app.py
@@ -2,7 +2,7 @@
 # @Author: Climax
 # @Date:   2022-07-09 22:31:13
 # @Last Modified by:   Climax
-# @Last Modified time: 2022-07-15 23:33:36
+# @Last Modified time: 2022-07-17 01:01:01
 
 
 import sys 
@@ -34,22 +34,22 @@ class MainWindow(QMainWindow):
 		# first label
 		label_Intro = QLabel("Enter your Temperature value below~", self)
 		label_Intro.setFont(QFont("Arial", 15))
-		label_Intro.resize(450,30)
+		label_Intro.resize(500,30)
 		label_Intro.move(20,20)
 
 		# second label
 		label_description = QLabel("Please insert your value in one of the boxes given below. The application will display the converted value of the other corresponding values.", self)
 		label_description.setFont(QFont("Arial", 15))
-		label_description.resize(550,90)
+		label_description.resize(500,90)
 		label_description.move(20,50)
 		label_description.setWordWrap(True)
 
 	def input_field(self):
 		# Labels for the SI Units
 		# dict - units, dimensions
-		temperatures = {"Celcius" : 200, 
-						"Fahrenheit" : 350, 
-						"Kelvin" : 500}
+		temperatures = {"Celcius" : 180, 
+						"Fahrenheit" : 330, 
+						"Kelvin" : 480}
 
 		for unit,dimensions in temperatures.items():
 			labels = QLabel("*"+unit+":", self)
@@ -59,16 +59,17 @@ class MainWindow(QMainWindow):
 
 		# input fields
 		self.input_Celcius = QLineEdit("", self)
-		self.input_Celcius.move(20,265)
+		self.input_Celcius.move(20,245)
 
 		self.input_Fahrenheit = QLineEdit("", self)
-		self.input_Fahrenheit.move(20,415)
+		self.input_Fahrenheit.move(20,390)
 
 		self.input_Kelvin = QLineEdit("", self)
-		self.input_Kelvin.move(20,570)
+		self.input_Kelvin.move(20,540)
 
 		for size in self.input_Kelvin, self.input_Fahrenheit, self.input_Celcius:
-			size.resize(300,50)
+			# set size for QLineEdit
+			size.resize(500,50)
 
 			font = size.font()
 			font.setPointSize(16)
